b2e253e3a67c13a6b917bc1ec7901795299cbce6
MiklosWassel <wassel.mikee7@gmail.comcd/home/tanulok/wasmik9592/Dokumentumok git config --global user.email wassel.mikee7@gmail.com>
2023-09-11 15:53:19 +0200

    first commit

---
+++ egy.py
@@ -0,0 +1,30 @@
+print("Szia, üdv itt!")
+print("Ez egy",end=" ****** ")
+print("program")
+print('*'*10)
+print('alma '*10) # ez működik
+#print('mama'/2)  hibás utasítás
+#print("mama"-"ma")
+#print("mama"-2)
+#print("mama"**3)
+#print("mama"^3)
+print('-'*10)
+print('*'*-10) # mintha print() lenne.
+print('-'*-10)
+
+#print("al\n\n\nma")
+#print("a\nl\nm\na")
+print("al\nma") # új sor
+print("al\rma") # az al eltünik
+print("al\fma") # új sor + szóköz
+print("al\bma") # az l eltünik visszatörlés
+print("al\tma") # tabulátor a szó közepén
+print("al\vma") # sor emelés
+print("al\ama") #teljesen ki írja szót 
+print('\'')
+print('\\') # backslash
+print('\\')
+print('\'')
+print('\x33') # karakter kód az 'x' után  
+
+
