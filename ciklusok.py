556c83eb4e86689816b42b13b43a512c92e75eef
MiklosWassel <wassel.mikee7@gmail.comcd/home/tanulok/wasmik9592/Dokumentumok git config --global user.email wassel.mikee7@gmail.com>
2023-10-02 14:02:36 +0200

    c

---
+++ ciklusok.py
@@ -0,0 +1,47 @@
+szamok = [1,2,33,6,9]
+# pl java script:
+#for (i=0;i<5;i++) {...ciklus mag ...}
+for szam in szamok:
+    print(szam)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
